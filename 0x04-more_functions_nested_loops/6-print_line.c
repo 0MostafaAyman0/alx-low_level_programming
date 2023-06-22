d0fc614f1ca4c3e048736d033407dabb4f1b7196
0MostafaAyman0 <mostafayma123@gmail.com>
2023-06-22 12:52:15 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,17 @@ include "main.h"
  */
 void print_line(int n)
 {
-	if (n <= 0)
+		if (n <= 0)
 	{
 		_putchar('\n');
-	} else
+	}	 else
 	{
 		int i;
-
+	}
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+		_putchar('_');
 		}
 		_putchar('\n');
 	}
-
 }
