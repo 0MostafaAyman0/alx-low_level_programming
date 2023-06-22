9bba399206fd1f1b025a92f42718924eaa1bc168
0MostafaAyman0 <mostafayma123@gmail.com>
2023-06-22 13:09:31 -0700

    fix 2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_line - draws a straight line in the terminal
@@ -6,17 +6,20 @@ include "main.h"
  */
 void print_line(int n)
 {
-		if (n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
-	}	 else
+	else
 	{
 		int i;
 	}
+
 		for (i = 1; i <= n; i++)
 		{
-		_putchar('_');
+			_putchar('_');
 		}
+	{
 		_putchar('\n');
 	}
+
 }
